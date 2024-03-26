61a311ab2f5dc3775a3f62cbcc6b577c8a9da5cb
adithya-aiplanet <156287566+adithya-aiplanet@users.noreply.github.com>
2024-03-26 17:40:50 +0530

    Hide keys

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from llama_index.embeddings.azure_openai import AzureOpenAIEmbedding
 from llama_index.core import VectorStoreIndex, ServiceContext
 
 
-# data = fit(path="/home/adithya/Downloads/Example_doc.pdf",loader_type='llama-parse',chunk_size=512,chunk_overlap=50,llama_parse_key="llx-fONiYabJEOhMhJ3FOfiI9tGTHzrJRdkGnyzyO1UQFAjzsoxH")
+# data = fit(path="/home/adithya/Downloads/Example_doc.pdf",loader_type='llama-parse',chunk_size=512,chunk_overlap=50,llama_parse_key="llx-*************")
 data = fit(path="/home/adithya/Downloads/AdithyaHegde_Resume.pdf",chunk_size=512,chunk_overlap=50)
 print(data)
 print(type(data))
@@ -54,11 +54,11 @@ print(response)
 
 
 
-# os.environ["AZURE_OPENAI_API_KEY"] = "d6d9522a01c74836907af2f3fd72ff85"
+# os.environ["AZURE_OPENAI_API_KEY"] = "**************"
 # os.environ["AZURE_OPENAI_ENDPOINT"] = "https://marketplace.openai.azure.com/"
 
 # azure_embeddings = AzureOpenAIEmbedding(
-#             api_key="d6d9522a01c74836907af2f3fd72ff85",
+#             api_key="**********",
 #             azure_endpoint="https://marketplace.openai.azure.com/",
 #             deployment_name = "text-embed-marketplace",
 #             api_version="2023-05-15",
@@ -66,7 +66,7 @@ print(response)
 # llm = AzureOpenAI(
 #     model="gpt-35-turbo-16k",
 #     deployment_name="gpt4-inference",
-#     api_key="a20bc67dbd7c47ed8c978bbcfdacf930",
+#     api_key="**********",
 #     azure_endpoint="https://gpt-res.openai.azure.com/",
 #     api_version="2023-07-01-preview",
 # )
