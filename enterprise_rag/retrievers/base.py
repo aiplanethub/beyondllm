e19b33e0f9d8675181e1854cf796892c8f5211dc
Adithya Hegde <hedgeaditya39@gmail.com>
2024-03-27 18:30:58 +0530

    Added retriever, evaluation

---
+++ enterprise_rag/retrievers/base.py
@@ -0,0 +1,21 @@
+from .evaluate import evaluate_retriever
+
+class BaseRetriever:
+    def __init__(self, data, embed_model,**kwargs):
+        self.data = data
+        self.embedder = embed_model
+
+    def load_index(self):
+        raise NotImplementedError("This method should be implemented by subclasses.")
+    
+    def as_retriever(self):
+        raise NotImplementedError("This method should be implemented by subclasses.")
+    
+    def retrieve(self,query):
+        retriever = self.as_retriever()
+        return retriever.retrieve(query)
+
+    def evaluate(self,llm):
+        nodes = self.data
+        retriever = self.as_retriever()
+        return evaluate_retriever(llm,nodes,retriever)
