97803af120c70cbbb51df4a63ab5cb6925782c6a
adithya-aiplanet <156287566+adithya-aiplanet@users.noreply.github.com>
2024-04-03 14:16:58 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,11 +5,11 @@ from enterprise_rag.retrieve import auto_retriever
 # from llama_index.llms.azure_openai import AzureOpenAI
 
 
-# data = fit(path="/home/adithya/Downloads/Example_doc.pdf",loader_type='llama-parse',chunk_size=512,chunk_overlap=50,llama_parse_key="llx-fONiYabJEOhMhJ3FOfiI9tGTHzrJRdkGnyzyO1UQFAjzsoxH")
+# data = fit(path="/home/adithya/Downloads/Example_doc.pdf",loader_type='llama-parse',chunk_size=512,chunk_overlap=50,llama_parse_key="llx-**********")
 data = fit(path="https://www.youtube.com/watch?v=N2Ul-KWJdHw",loader_type="youtube",chunk_size=100,chunk_overlap=50)
 # print(data)
 
-embed_model = OpenAIEmbeddings(api_key="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")
+embed_model = OpenAIEmbeddings(api_key="sk-************")
 
 retriever = auto_retriever(data,embed_model,type="cross-rerank",top_k=7)
 
@@ -18,7 +18,7 @@ print(retriever.retrieve("Spinners")[0])
 
 # embed_model = get_embed_model(
 #     model_name="AzureOpenAI",
-#     api_key="a20bc67dbd7c47ed8c978bbcfdacf930",
+#     api_key="***********",
 #     azure_endpoint="https://marketplace.openai.azure.com/",
 #     deployment_name = "text-embed-marketplace",
 #     api_version="2023-05-15",
@@ -29,7 +29,7 @@ print(retriever.retrieve("Spinners")[0])
 # llm = AzureOpenAI(
 #     model="gpt-35-turbo-16k",
 #     deployment_name="gpt4-inference",
-#     api_key="a20bc67dbd7c47ed8c978bbcfdacf930",
+#     api_key="************",
 #     azure_endpoint="https://gpt-res.openai.azure.com/",
 #     api_version="2023-07-01-preview",
 # )
