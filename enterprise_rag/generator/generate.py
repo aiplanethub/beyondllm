1d4f88d8f86b7a8959dce82749830c02e1b00d6a
taham655 <taham655@gmail.com>
2024-04-06 12:40:09 +0800

    Adding evaluation to generate

--- enterprise_rag/generator/generate.py
+++ enterprise_rag/generator/generate.py
@@ -1,8 +1,10 @@
 from enterprise_rag.llms import ChatOpenAIModel
-from enterprise_rag.utils import CONTEXT_RELEVENCE,GROUNDEDNESS,ANSWER_RELEVENCE
+from enterprise_rag.utils import CONTEXT_RELEVENCE,GROUNDEDNESS,ANSWER_RELEVENCE, GROUND_TRUTH
 
 import os,re
 import numpy as np
+from typing import List
+import pysbd
 from .base import BaseGenerator,GeneratorConfig
 from dataclasses import dataclass,field
 
@@ -17,6 +19,10 @@ def extract_number(response):
     if match:
         return int(match.group(0))
     return np.nan
+
+def sent_tokenize(text: str) -> List[str]:
+    seg = pysbd.Segmenter(language="en", clean=False)
+    return seg.segment(text)
 
 @dataclass
 class Generate:
@@ -61,9 +67,10 @@ class Generate:
         return self.RESPONSE
 
     def get_rag_triad_evals(self):
-        context_relevancy = None
-        answer_relevancy = None
-        groundness = None
+        context_relevancy = self.get_context_relevancy()
+        answer_relevancy = self.get_answer_relevancy()
+        groundness = self.get_groundness()
+        return context_relevancy,answer_relevancy,groundness
         
     def get_context_relevancy(self):
         total_score = 0
@@ -83,10 +90,40 @@ class Generate:
         return f"Context relevancy Score: {average_score}"
 
     def get_answer_relevancy(self):
-        return f"Answer relevancy Score: {ANSWER_RELEVENCE(self.RESPONSE,self.question)}"
-
+        try:
+            score_str = self.llm.predict(ANSWER_RELEVENCE.format(question=self.question, context= self.RESPONSE))
+            score = float(extract_number(score_str))
+            return f"Answer relevancy Score: {score}"
+        except Exception as e:
+            print(f"Failed during answer relevance evaluation: {e}")
+        
     def get_groundness(self):
-        return f"Groundness score: {GROUNDEDNESS(self.response,self.question)}"
+        try:
+            statements = sent_tokenize(self.RESPONSE)
+            scores = []
+            for statement in statements:
+
+                score_response = self.llm.predict(GROUNDEDNESS.format(statement=statement, context=self.CONTEXT))
+                score = extract_number(score_response)
+
+                print("Statement:", statement)
+                print("Score:", score)
+                scores.append(score)
+                
+            average_score = sum(scores) / len(scores) if scores else 0
+            return f"Groundness score: {average_score}"
+        
+        except Exception as e:
+            raise Exception("Failed during groundedness evaluation: ", str(e))
+
+    def get_ground_truth(self, answer:str):
+        if answer is None:
+            print("Error: Missing 'answer' parameter. Usage: get_ground_truth(answer=\"your_answer_here\").")
+            return
+        score_str = self.llm.predict(GROUND_TRUTH.format(ground_truth=answer, generated_response=self.RESPONSE))
+        score = extract_number(score_str)
+        return f"Ground truth score: {score}"
+        
 
     @staticmethod
     def load_from_kwargs(self,kwargs): 
