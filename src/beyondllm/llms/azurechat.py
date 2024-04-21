59f0513deb87d1506251e7beb1d32cec1573bd36
lucifertrj <tarun@aiplanet.com>
2024-04-21 13:58:51 +0530

    env variable setup for Azure embed and llm

--- src/beyondllm/llms/azurechat.py
+++ src/beyondllm/llms/azurechat.py
@@ -1,6 +1,7 @@
 from beyondllm.llms.base import BaseLLMModel, ModelConfig
 from typing import Any, Dict, List, Optional
 from dataclasses import dataclass
+import os
 
 @dataclass
 class AzureOpenAIModel:
@@ -8,15 +9,23 @@ class AzureOpenAIModel:
     Class representing a Chat Language Model (LLM) model using OpenAI
     Example:
     >>> from beyondllm.llms import ChatOpenAIModel
-    >>> llm = AzureOpenAIModel(model="gpt4",api_key = "<your_api_key>",deployment_name="",endpoint_url="",model_kwargs={"max_tokens":512,"temperature":0.1})
+    >>> llm = AzureOpenAIModel(model="gpt4",azure_key = "<your_api_key>",deployment_name="",endpoint_url="",model_kwargs={"max_tokens":512,"temperature":0.1})
     """
-    api_key: str
     deployment_name: str
-    endpoint_url: str
+    azure_key: str = ""
+    endpoint_url: str = ""
     model: str = "gpt4"
     model_kwargs: Optional[Dict] = None
 
     def __post_init__(self):
+        if not self.azure_key: 
+            self.azure_key = os.getenv('AZURE_KEY_LLM') 
+            if not self.azure_key: 
+                raise ValueError("AZURE_KEY_LLM is not provided and not found in environment variables.")
+        if not self.endpoint_url:
+            self.endpoint_url = os.getenv("ENDPOINT_URL_LLM")
+            if not self.endpoint_url: 
+                raise ValueError("ENDPOINT_URL_LLM is not provided and not found in environment variables.")
         self.load_llm()
 
     def load_llm(self):
@@ -30,7 +39,7 @@ class AzureOpenAIModel:
                 api_version = "2023-05-15",
                 azure_endpoint = self.endpoint_url,
                 azure_deployment = self.deployment_name,  
-                api_key = self.api_key
+                api_key = self.azure_key
             )
         except Exception as e:
             raise Exception("Failed to load the model from Azure OpenAI:", str(e))
@@ -41,7 +50,7 @@ class AzureOpenAIModel:
             response = self.client.chat.completions.create(
                 model = self.model,
                 messages=[
-                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "system", "content": "You are an AI assitant that is using Azure OpenAI library. You are a GPT-4 Large Language model that is the backbone that helps with user prompts."},
                     {"role": "user", "content": prompt}
                 ],
             **self.model_kwargs
@@ -50,7 +59,7 @@ class AzureOpenAIModel:
             response = self.client.chat.completions.create(
                 model = self.model,
                 messages=[
-                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "system", "content": "You are an AI assitant that is using Azure OpenAI library. You are a GPT-4 Large Language model that is the backbone that helps with user prompts."},
                     {"role": "user", "content": prompt}
                 ]
             )
