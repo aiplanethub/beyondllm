ce885b133e39855306400b206f6759888e03cc87
adithya-aiplanet <hedgeaditya39@gmail.com>
2024-05-24 18:17:45 +0530

    Name change

--- src/beyondllm/llms/__init__.py
+++ src/beyondllm/llms/__init__.py
@@ -5,4 +5,4 @@ from .chatopenai import ChatOpenAIModel
 from .azurechat import AzureOpenAIModel
 from .ollama import OllamaModel
 from .multimodal import GeminiMultiModal
-from .gpt4o import GPT4OpenAIModel
+from .gpt4o import GPT4oOpenAIModel
