4bbbc652045bb6317a8a267457dfac84a33f9f61
jaywyawhare <2005441@kiit.ac.in>
2024-07-12 23:04:43 +0530

    Change the default Ollama server URL under LLM

--- src/beyondllm/llms/ollama.py
+++ src/beyondllm/llms/ollama.py
@@ -10,23 +10,25 @@ class OllamaModel:
     ## Example:
     ```
     >>> from beyondllm.llms import OllamaModel
-    >>> llm = OllamaModel(model="llama2")
+    >>> llm = OllamaModel(model="llama2", base_url="http://localhost:11434")
     ```
     """
     model: str
+    base_url: Optional[str] = "http://localhost:11434"
 
     def __post_init__(self):
         self.load_llm()
         
+        
     def load_llm(self):
         try:
             import ollama
-            self.client = ollama.Client(host='http://localhost:11434')
+            self.client = ollama.Client(host=self.base_url)
 
         except ImportError:
             raise ImportError("Ollama library is not installed. Please install it with `pip install ollama`.")
         except ConnectionError:
-            raise ConnectionError("Ollama by default runs on LocalHost:11434")
+            raise ConnectionError(f"Cannot connect to Ollama at {self.base_url}. Ensure Ollama is running at this URL.")
 
     def predict(self, prompt: Any):
         try:
