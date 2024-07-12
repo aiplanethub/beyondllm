0da852de5ba38f4033c6b32f45add430965ab91a
jaywyawhare <2005441@kiit.ac.in>
2024-07-12 23:17:16 +0530

    Feat : Addition of new LLM evals metrics

--- src/beyondllm/generator/generate.py
+++ src/beyondllm/generator/generate.py
@@ -1,34 +1,46 @@
 from beyondllm.llms import GeminiModel
-from beyondllm.utils import CONTEXT_RELEVENCE,GROUNDEDNESS,ANSWER_RELEVENCE, GROUND_TRUTH
+from beyondllm.utils import (
+    CONTEXT_RELEVENCE,
+    GROUNDEDNESS,
+    ANSWER_RELEVENCE,
+    GROUND_TRUTH,
+)
 
-import os,re
+import os, re
 import numpy as np
 from typing import List
 import pysbd
-from .base import BaseGenerator,GeneratorConfig
-from dataclasses import dataclass,field
+from .base import BaseGenerator, GeneratorConfig
+from dataclasses import dataclass, field
+
 
 def default_llm():
-    api_key = os.getenv('GOOGLE_API_KEY')
+    api_key = os.getenv("GOOGLE_API_KEY")
     if not api_key:
-        raise EnvironmentError("GOOGLE_API_KEY must be set in the environment variables.")
+        raise EnvironmentError(
+            "GOOGLE_API_KEY must be set in the environment variables."
+        )
     return GeminiModel(google_api_key=api_key)
 
+
 def extract_number(response):
-    match = re.search(r'\b(10|[0-9])\b', response)
+    match = re.search(r"\b(10|[0-9])\b", response)
     if match:
         return int(match.group(0))
     return np.nan
 
+
 def sent_tokenize(text: str) -> List[str]:
     seg = pysbd.Segmenter(language="en", clean=False)
     return seg.segment(text)
+
 
 def thresholdCheck(score):
     if score >= 8:
         return "This response meets the evaluation threshold. It demonstrates strong comprehension and coherence."
     else:
         return "This response does not meet the evaluation threshold. Consider refining the structure and content for better clarity and effectiveness."
+
 
 @dataclass
 class Generate:
@@ -48,16 +60,20 @@ class Generate:
     >>> groundness = pipeline.get_groundedness()
     ```
     """
+
     question: str
     system_prompt: str = None
-    retriever:str = ''
+    retriever: str = ""
     llm: GeminiModel = field(default_factory=default_llm)
-    
+
     def __post_init__(self):
         self.pipeline()
 
     def pipeline(self):
-        self.CONTEXT = [node_with_score.node.text for node_with_score in self.retriever.retrieve(self.question)]
+        self.CONTEXT = [
+            node_with_score.node.text
+            for node_with_score in self.retriever.retrieve(self.question)
+        ]
         temp = ".".join(self.CONTEXT)
 
         if self.system_prompt is None:
@@ -77,12 +93,12 @@ class Generate:
         """
 
         self.RESPONSE = self.llm.predict(template)
-        return self.CONTEXT,self.RESPONSE
+        return self.CONTEXT, self.RESPONSE
 
     def call(self):
         return self.RESPONSE
 
-    def get_rag_triad_evals(self, llm = None):
+    def get_rag_triad_evals(self, llm=None):
         if llm is None:
             llm = self.llm
         print("Executing RAG Triad Evaluations...")
@@ -90,16 +106,18 @@ class Generate:
         answer_relevancy = self.get_answer_relevancy(llm)
         groundness = self.get_groundedness(llm)
         return f"{context_relevancy}\n{answer_relevancy}\n{groundness}"
-        
-    def get_context_relevancy(self, llm = None):
+
+    def get_context_relevancy(self, llm=None):
         if llm is None:
             llm = self.llm
-            
+
         total_score = 0
         score_count = 0
-        
+
         for context in self.CONTEXT:
-            score_str = llm.predict(CONTEXT_RELEVENCE.format(question=self.question, context=context))
+            score_str = llm.predict(
+                CONTEXT_RELEVENCE.format(question=self.question, context=context)
+            )
             score = float(extract_number(score_str))
             total_score += score
             score_count += 1
@@ -111,18 +129,20 @@ class Generate:
 
         return f"Context relevancy Score: {round(average_score, 1)}\n{thresholdCheck(average_score)}"
 
-    def get_answer_relevancy(self, llm = None):
+    def get_answer_relevancy(self, llm=None):
         if llm is None:
             llm = self.llm
         try:
-            score_str = llm.predict(ANSWER_RELEVENCE.format(question=self.question, context= self.RESPONSE))
+            score_str = llm.predict(
+                ANSWER_RELEVENCE.format(question=self.question, context=self.RESPONSE)
+            )
             score = float(extract_number(score_str))
-    
+
             return f"Answer relevancy Score: {round(score, 1)}\n{thresholdCheck(score)}"
         except Exception as e:
-            return(f"Failed during answer relevance evaluation: {e}")
-        
-    def get_groundedness(self, llm = None):
+            return f"Failed during answer relevance evaluation: {e}"
+
+    def get_groundedness(self, llm=None):
         if llm is None:
             llm = self.llm
         try:
@@ -130,31 +150,64 @@ class Generate:
             scores = []
             for statement in statements:
 
-                score_response = llm.predict(GROUNDEDNESS.format(statement=statement, context=self.CONTEXT))
+                score_response = llm.predict(
+                    GROUNDEDNESS.format(statement=statement, context=self.CONTEXT)
+                )
                 score = extract_number(score_response)
                 scores.append(score)
-                
+
             average_score = sum(scores) / len(scores) if scores else 0
-            
+
             return f"Groundness score: {round(average_score, 1)}\n{thresholdCheck(average_score)}"
-        
+
         except Exception as e:
             raise Exception("Failed during groundedness evaluation: ", str(e))
 
-    def get_ground_truth(self, answer:str, llm = None):
+    def get_ground_truth(self, answer: str, llm=None):
         if llm is None:
             llm = self.llm
         if answer is None:
-            print("Error: Missing 'answer' parameter. Usage: get_ground_truth(answer=\"your_answer_here\").")
+            print(
+                "Error: Missing 'answer' parameter. Usage: get_ground_truth(answer=\"your_answer_here\")."
+            )
             return
-        score_str = llm.predict(GROUND_TRUTH.format(ground_truth=answer, generated_response=self.RESPONSE))
+        score_str = llm.predict(
+            GROUND_TRUTH.format(ground_truth=answer, generated_response=self.RESPONSE)
+        )
         score = extract_number(score_str)
-        
+
         return f"Ground truth score: {round(score, 1)}\n{thresholdCheck(score)}"
-        
+
+    def get_faithfulness(self, llm=None):
+        if llm is None:
+            llm = self.llm
+        try:
+            score_str = llm.predict(
+                FAITHFULNESS.format(statement=self.RESPONSE, context=self.CONTEXT)
+            )
+            score = float(extract_number(score_str))
+
+            return f"Faithfulness score: {round(score, 1)}\n{thresholdCheck(score)}"
+
+        except Exception as e:
+            return f"Failed during faithfulness evaluation: {e}"
+
+    def get_correctness(self, llm=None):
+        if llm is None:
+            llm = self.llm
+        try:
+            score_str = llm.predict(
+                CORRECTNESS.format(statement=self.RESPONSE, context=self.CONTEXT)
+            )
+            score = float(extract_number(score_str))
+
+            return f"Correctness score: {round(score, 1)}\n{thresholdCheck(score)}"
+
+        except Exception as e:
+            return f"Failed during correctness evaluation: {e}"
 
     @staticmethod
-    def load_from_kwargs(self,kwargs): 
+    def load_from_kwargs(self, kwargs):
         model_config = GeneratorConfig(**kwargs)
         self.config = model_config
         self.pipeline()
